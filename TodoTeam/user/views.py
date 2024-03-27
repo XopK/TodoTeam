663d0363c2b0c4700f338e1e98b019bb0b4cf9af
KirillHorn <122849903+KirillHorn@users.noreply.github.com>
2024-03-27 20:22:17 +0500

    Сверстана главная страница

--- TodoTeam/user/views.py
+++ TodoTeam/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def registration(request):
+    return render(request, 'user/Registration.html')
+
+def entrance(request):
+    return render(request, 'user/entrance.html')
