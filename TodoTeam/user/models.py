62ca6e5052b45e8dbf5665262d42106e440bb30e
XopK <dmahmutov12@gmail.com>
2024-03-27 17:32:14 +0500

    models

--- TodoTeam/user/models.py
+++ TodoTeam/user/models.py
@@ -1,4 +1,39 @@
 from django.db import models
+from django.contrib.auth import settings
 
 class Command(models.Model):
     name_command = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name_command
+
+    class Meta:
+        verbose_name_plural = "Команды"
+        verbose_name = "Команда"
+
+class CommandUser(models.Model):
+    command = models.ForeignKey('Command', on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        command_name = self.command.name_command if self.command else 'Нет команды'
+        user_name = f'{self.user.first_name} {self.user.last_name}' if self.user else 'Нет участника'
+        return f'{command_name} - {user_name}'
+
+    class Meta:
+        verbose_name_plural = "Список команд"
+        verbose_name = "Список"
+
+class favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    task = models.ForeignKey('task.Task', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        task_title = self.task.title_task if self.task else 'Нет задачи'
+        user_name = self.user.username if self.user else 'Нет пользователя'
+        return f'{task_title} - {user_name}'
+
+    class Meta:
+        verbose_name_plural = "Избранные"
+        verbose_name = "Избранное"
